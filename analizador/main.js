cae69f8a505ac31194f26555ddabc155727ddabc
Jonathan Ramiro Ruiz Sandoval <yoni_12rrs@hotmail.com>
2019-11-29 11:19:11 -0600

    work

--- analizador/main.js
+++ analizador/main.js
@@ -2,7 +2,8 @@ import Arbol from "./Arbol.js";
 
 let resultadoPre = document.querySelector("#resultadoPre");
 let resultadoPost = document.querySelector("#resultadoPost");
-class main{
+let resultadoPreOperacion = document.querySelector("#resultadoPreOperacion");
+class main {
     constructor() {
         let arbolBin = new Arbol();
         document.querySelector("#btn").addEventListener("click", () => {
@@ -14,6 +15,38 @@ class main{
             arbolBin.preOrder();
             console.log(arbolBin.preOrder());
             resultadoPre.innerHTML = "PreOrder: " + arbolBin.preOrder();
+            let operacion = arbolBin.preOrder().split(",");
+            let pila = [];
+            operacion.pop();
+            let inicio = 0;
+            for (let o = operacion.length - 1; i >= 0; i--) {
+                switch (operacion[i]) {
+                    case "*":
+                        inicio = pila.pop();
+                        inicio = inicio * pila.pop();
+                        pila.push(inicio);
+                        break;
+                    case "/":
+                        inicio = pila.pop();
+                        inicio = inicio / pila.pop();
+                        pila.push(inicio);
+                        break;
+                    case "+":
+                        inicio = pila.pop();
+                        inicio = inicio + pila.pop();
+                        pila.push(inicio);
+                        break;
+                    case "-":
+                        inicio = pila.pop();
+                        inicio = inicio - pila.pop();
+                        pila.push(inicio);
+                        break;
+                    default:
+                        pila.push(parseInt(operacion[i]));
+                        break;
+                }
+            }
+            resultadoPreOperacion.innerHTML("Resultado: " + pila[0]);
         });
         document.querySelector("#btnPost").addEventListener("click", () => {
             arbolBin.postOrder();
